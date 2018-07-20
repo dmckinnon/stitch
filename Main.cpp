bea19ccb4e75c7e5f653fe347d09944e41f3134e
damckinnon <insaynity@gmail.com>
2018-07-20 16:48:39 -0700

    Finished iteration 1 of FAST feature detector

--- Main.cpp
+++ Main.cpp
@@ -9,15 +9,16 @@ using namespace cv;
 using namespace std;
 
 #define FAST_SPACING 3
-
+#define THRESH 15
+/*
 struct point
 {
 	int x;
 	int y;
-};
+};*/
 
 /* Function prototypes */
-bool FindFASTFeatures(Mat img, vector<point>& features);
+bool FindFASTFeatures(Mat img, vector<Point>& features);
 
 bool ThreeOfFourValuesBrighterOrDarker(int i1, int i5, int i9, int i13, int pb, int p_b);
 
@@ -53,6 +54,9 @@ int main(int argc, char** argv)
 	Log:
 	- Starting with the theory of FAST features
 	- implementing fast features soon i hope, iteration 1
+	- FAST features basic version implemented with a threshold of 10. Do we use a dynamic
+	  threshold? I'll experiment with different numbers. 15 is working for now.
+	- Now score each feature with Shi-Tomasi score (then make descriptors)
 	*/
 
 	// pull in both images
@@ -60,9 +64,27 @@ int main(int argc, char** argv)
 	// TODO: make sure these are black and white
 	Mat leftImage = imread("C:\\Users\\d_mcc\\source\\adobe_panoramas\\data\\goldengate\\goldengate-00.png");
 	Mat rightImage = imread("C:\\Users\\d_mcc\\source\\adobe_panoramas\\data\\goldengate\\goldengate-01.png");
-	
-	// Find features in each image
 
+	// Find features in each image
+	vector<Point> leftFeatures;
+	if (FindFASTFeatures(leftImage, leftFeatures))
+	{
+		// Draw the features on the image
+		for (unsigned int i = 0; i < leftFeatures.size(); ++i)
+		{
+			circle(leftImage, leftFeatures[i], 2, (255, 255, 0), -1);
+		}
+
+		// Debug display
+		std::string debugWindowName = "debug image";
+		namedWindow(debugWindowName);
+		imshow(debugWindowName, leftImage);
+		waitKey(0);
+	}
+	else
+	{
+		cout << "Failed to find features in left image" << endl;
+	}
 
 
 	// Display for debug reasons
@@ -91,19 +113,18 @@ int main(int argc, char** argv)
    We start with a threshold of 10?
    Something to experiment with
 */
-#define THRESH 10
-bool FindFASTFeatures(Mat img, vector<point>& features)
+bool FindFASTFeatures(Mat img, vector<Point>& features)
 {
 	// For each pixel (three in from each side)
 	int width = img.cols;
 	int height = img.rows;
-	for (int h = FAST_SPACING; h < height- FAST_SPACING; ++h)
+	for (int h = FAST_SPACING; h < height - FAST_SPACING; ++h)
 	{
 		for (int w = FAST_SPACING; w < width - FAST_SPACING; ++w)
 		{
 			// Adapt a brightness threshold?
 
-			int p = img.at<int>(w, h);
+			int p = img.at<uchar>(h, w); // wrong number of dims ... 
 			int pb = p + THRESH;
 			int p_b = p - THRESH;
 
@@ -111,10 +132,10 @@ bool FindFASTFeatures(Mat img, vector<point>& features)
 
 			// For a speed-up, check 1, 9, then 5, 13
 			// Any three of 1,5,9,13 can be all brighter or darker. If not ... not a corner
-			int i1 = img.at<int>(w,h-FAST_SPACING);
-			int i5 = img.at<int>(w+FAST_SPACING, h);
-			int i9 = img.at<int>(w, h + FAST_SPACING);
-			int i13 = img.at<int>(w-FAST_SPACING, h);
+			int i1 = img.at<uchar>(h-FAST_SPACING, w);
+			int i5 = img.at<uchar>(h, w + FAST_SPACING);
+			int i9 = img.at<uchar>(h + FAST_SPACING, w);
+			int i13 = img.at<uchar>(h, w - FAST_SPACING);
 			if (!ThreeOfFourValuesBrighterOrDarker(i1, i5, i9, i13, pb, p_b))
 			{
 				continue;
@@ -123,18 +144,18 @@ bool FindFASTFeatures(Mat img, vector<point>& features)
 				// Now check the rest
 				// need 12 or more sequential values above or below
 				// First, get all the values
-				int i2 = img.at<int>(w+1, h - FAST_SPACING);
-				int i3 = img.at<int>(w+2, h - 2);
-				int i4 = img.at<int>(w + FAST_SPACING, h-1);
-				int i6 = img.at<int>(w + FAST_SPACING, h+1);
-				int i7 = img.at<int>(w + 2, h + 2);
-				int i8 = img.at<int>(w-1, h + FAST_SPACING);
-				int i10 = img.at<int>(w+1, h + FAST_SPACING);
-				int i11 = img.at<int>(w - 2, h + 2);
-				int i12 = img.at<int>(w - FAST_SPACING, h+1);
-				int i14 = img.at<int>(w - FAST_SPACING, h-1);
-				int i15 = img.at<int>(w - 2, h - 2);
-				int i16 = img.at<int>(w - 1, h - FAST_SPACING);
+				int i2 = img.at<uchar>(h - FAST_SPACING, w + 1);
+				int i3 = img.at<uchar>(h - 2, w + 2);
+				int i4 = img.at<uchar>(h-1, w + FAST_SPACING);
+				int i6 = img.at<uchar>(h+1, w + FAST_SPACING);
+				int i7 = img.at<uchar>(h + 2, w + 2);
+				int i8 = img.at<uchar>(h + FAST_SPACING, w - 1);
+				int i10 = img.at<uchar>(h + FAST_SPACING, w + 1);
+				int i11 = img.at<uchar>(h + 2, w - 2);
+				int i12 = img.at<uchar>(h+1, w - FAST_SPACING);
+				int i14 = img.at<uchar>(h-1, w - FAST_SPACING);
+				int i15 = img.at<uchar>(h - 2, w - 2);
+				int i16 = img.at<uchar>(h - FAST_SPACING, w - 1);
 				std::vector<int> points{ i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16 };
 
 				// Pass values into evaluation function
@@ -143,11 +164,12 @@ bool FindFASTFeatures(Mat img, vector<point>& features)
 					continue;
 				}
 
-				// We have a feature
+				// We have a feature. Mark this spot
+				Point feature;
+				feature.x = w;
+				feature.y = h;
+				features.push_back(feature);
 			}
-			
-
-			// We didn't fail the check
 		}
 	}
 
@@ -196,6 +218,17 @@ bool ThreeOfFourValuesBrighterOrDarker(int i1, int i5, int i9, int i13, int pb, 
 	return false;
 }
 
+// Helper functions for FAST features
+bool greaterthan(int i, int pb, int p_b)
+{
+	return i > pb;
+}
+
+bool lessthan(int i, int pb, int p_b)
+{
+	return i < p_b;
+}
+
 /*
 	If there is a sequence of i values that are all above pb or below p_b, return true.
 	Else, return false.
@@ -210,11 +243,56 @@ bool CheckForSequential12(std::vector<int> points, int p_b, int pb)
 
 	// Yes, there are smarter ways to do this. No, I don't care right now.
 	int p = (pb + p_b) / 2;
-	for (unsigned int i = 0; i < points.size(); ++i)
-	{
-		
-		// Prolly easiest to just use a function pointer at this stage
 
+	bool(*comp)(int, int, int);
+	for (int i = 0; i < (int)points.size(); ++i)
+	{
+		if (points[i] > pb)
+		{
+			comp = &greaterthan;
+		}
+		else if (points[i] < p_b)
+		{
+			comp = &lessthan;
+		}
+		else {
+			continue;
+		}
+		
+		// Now loop over the rest of the sequence, forward and backward,
+		// until both sides return false
+		// Forward loop
+		int fLen = 0;
+		for (int j = i + 1; j!=i; ++j)
+		{
+			// quit when we get back to i
+			if (j == 16)
+				j = 0;
+			if (j == i)
+				break;
+			if (comp(points[j], pb, p_b))
+				fLen++;
+			else
+				break;
+		}
+		int bLen = 0;
+		for (int j = i - 1; j != 1; --j)
+		{
+			// quit when we get back to i
+			if (j == -1)
+				j = 15;
+			if (j == i)
+				break;
+			if (comp(points[j], pb, p_b))
+				bLen++;
+			else
+				break;
+		}
+		int seqLen = fLen + bLen + 1;
+		if (seqLen >= 12)
+		{
+			return true;
+		}
 	}
 
 	return false;
